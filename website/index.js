4dc9bf92b193f3c295a9049b8ae19ca679e0b59a
Devanshukumawat <128327394+Devanshukumawat@users.noreply.github.com>
2024-01-17 12:28:09 +0530

    Add files via upload

---
+++ website/index.js
@@ -0,0 +1,22 @@
+
+
+// i= false
+// j = false
+
+// if(i || j){
+//     console.log("Hello")
+// }else{
+//     console.log("false")
+// }
+
+// let isMarried = true
+
+// if(isMarried!==true){
+//     console.log("haa bhaii ho gai shaadi")
+// }
+// else{
+//     console.log("awwww")
+// }
+
+
+
